ea85b003671d88bb421cc5683e02e3d42da897d3
PanArtur <pankovets.artur1@gmail.com>
2026-05-10 23:21:28 +0300

    refactor: split styles into separate files by responsibility

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { loadComponent } from './js/loader';
 
-import './style.css';
+import './styles/main.css';
 
 await loadComponent("#header", "/src/components/header.html");
 await loadComponent("#hero", "/src/components/hero.html");

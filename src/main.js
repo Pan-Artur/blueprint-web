629592931efc12c17b1d95b8ece36a4f9fb16544
PanArtur <pankovets.artur1@gmail.com>
2026-05-10 15:23:01 +0300

    chore: add base component files and connect all sections in main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import { loadComponent } from './js/loader';
 import './style.css';
 
 await loadComponent("#header", "/src/components/header.html");
+await loadComponent("#hero", "/src/components/hero.html");
+await loadComponent("#services", "/src/components/services.html");
+await loadComponent("#about", "/src/components/about.html");
+await loadComponent("#advantages", "/src/components/advantages.html");
+await loadComponent("#cta", "/src/components/cta.html");
+await loadComponent("#footer", "/src/components/footer.html");
